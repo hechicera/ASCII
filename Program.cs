caa1b1ab520ad43367b84a0be89040c7e4ac742d
Maryna Bogdan <lagutinam@gmail.com>
2017-02-08 00:15:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII_letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Alphabet = new List<string>();
+            Alphabet.Add(" #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ");
+            Alphabet.Add("# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # ");
+            Alphabet.Add("### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   #  ");
+            Alphabet.Add("# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       ");
+            Alphabet.Add("# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  ");
+
+            int L = 4;
+            int H = 5;
+            int num = 0;
+            string T = Console.ReadLine();
+            char[] inputText = T.ToCharArray();
+
+            for (int j = 0; j < 5; j++)
+            {
+                string[] lineConstruct = new string[inputText.Length];
+                string[] heightConstruct = new string[H];
+                for (int i = 0; i < inputText.Length; i++)
+                {
+                    if (inputText[i] == 97 || inputText[i] == 123)
+                    {
+                        num = ((inputText[i] - 97) * L);
+                        lineConstruct[i] += Alphabet[j].Substring(num, L);
+                    }
+                    else
+                    if (inputText[i] > 97 && inputText[i] < 123)
+                    {
+                        num = ((inputText[i] - 97) * 4) - 1;
+                        lineConstruct[i] += Alphabet[j].Substring(num, L);
+                    }
+                    else
+                    if (inputText[i] < 97 || inputText[i] > 123)
+                    {
+                        lineConstruct[i] += Alphabet[j].Substring(103, L);
+                    }
+                    heightConstruct[j] += lineConstruct[i];
+                }
+                Console.WriteLine(heightConstruct[j]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
